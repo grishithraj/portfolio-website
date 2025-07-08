5dc7a0658aa400778fc5b84ad4e07e3833082882
grishithraj <rishithefx@gmail.com>
2025-07-08 20:25:37 +0530

    adde smooth scroll and bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,30 @@
 import { useEffect } from "react";
 import Home from "./pages/Home";
-// import Lenis from "@studio-freight/lenis";
-// import { SmoothCursor } from "./components/ui/smooth-cursor";
+import Lenis from "@studio-freight/lenis";
 
 function App() {
-  // useEffect(() => {
-  //   const lenis = new Lenis({
-  //     duration: 1.2,
-  //     easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-  //     orientation: "vertical",
-  //     gestureOrientation: "vertical",
-  //     smoothWheel: true,
-  //     touchMultiplier: 2,
-  //   });
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      orientation: "vertical",
+      gestureOrientation: "vertical",
+      smoothWheel: true,
+      touchMultiplier: 2,
+    });
 
-  //   function raf(time: number) {
-  //     lenis.raf(time);
-  //     requestAnimationFrame(raf);
-  //   }
+    function raf(time: number) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
 
-  //   requestAnimationFrame(raf);
+    requestAnimationFrame(raf);
 
-  //   // Cleanup
-  //   return () => {
-  //     lenis.destroy();
-  //   };
-  // }, []);
+    // Cleanup
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
   return (
     <>
       <div className="min-h-screen bg-gray-900  text-white transition-colors duration-300">
