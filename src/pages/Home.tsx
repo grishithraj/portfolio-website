f63e2ac0d45a6406030218e81c087e13b8a02e08
grishithraj <rishithefx@gmail.com>
2025-07-09 18:18:23 +0530

    added card

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,6 +4,7 @@ import Projects from "../components/project";
 import Contact from "../components/contact";
 import { ScrollProgress } from "../components/magicui/scroll-progress";
 import { TextReveal } from "../components/magicui/text-reveal";
+import { BackgroundGradient } from "../components/ui/background-gradient";
 
 function Home() {
   return (
@@ -11,7 +12,16 @@ function Home() {
       <div className="py-8">
         <NavbarDemo />
         <Hero />
+
         <TextReveal>Welcome to my portfolio!</TextReveal>
+        <BackgroundGradient>
+          {" "}
+          We help creators and businesses stand out with stunning videos and
+          modern websites. From engaging edits to responsive web design, we
+          craft visuals and experiences that tell your story and leave an
+          impact. Let’s bring your ideas to life!
+        </BackgroundGradient>
+
         <Projects />
         <Contact />
         <ScrollProgress />
